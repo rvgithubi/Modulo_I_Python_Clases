312ac50869832726662c762253c65de46db1d99f
Roger Vera <migu3l.v5@gmail.com>
2025-07-22 00:35:28 -0500

    Inicio de Proyecto

---
+++ Semena03/Practica01.py
@@ -0,0 +1,14 @@
+CURSOS CERSEU 10 FISI
+14:09
+git add .
+git commit -am"mensaje"
+git push origin master
+git status
+Meylin Kcomt
+14:09
+si por algun motivo deseo cambiarl el mensaje despues de colocado se puede?
+oki oki
+oki
+CURSOS CERSEU 10 FISI
+14:16
+git checkout -b develop
